6034db94de0f8d4dfe6f52b63f8e049badb38012
Prince <pkrkinth@gmail.com>
2021-05-02 00:35:34 +0530

    Added application code

--- wallpaperapp/src/index.js
+++ wallpaperapp/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let currentTime = new Date();
+currentTime = currentTime.toDateString();
+let morning = "00:00:00";
+let afternoon = "12:00:00";
+let night = "18:00:00";
+
+let greeting = " ";
+const cssStyle = { };
+
+if(currentTime >= morning && currentTime < afternoon) {
+  greeting = "Good Morning!";
+  cssStyle.color = "Green";
+}else if(currentTime >= afternoon && currentTime < night) {
+  greeting = "Good Afternoon!";
+  cssStyle.color = "Orange";
+}else {
+  greeting = "Good Night!";
+  cssStyle.color = "Blue";
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <div className = "backgroundImage">
+      <h1>Hello Koena, <span style = {cssStyle}> {greeting} </span></h1>
+    </div>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
