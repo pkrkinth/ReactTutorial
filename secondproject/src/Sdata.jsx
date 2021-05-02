e038133c1c2e8feefa7502ed66414622019096d7
Prince <pkrkinth@gmail.com>
2021-05-03 02:11:49 +0530

    updated index.css, Cards.jsx, Sdata.jsx  files

---
+++ secondproject/src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        sname: "Anupama",
+        imgsrc: "https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/6769/826769-h",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/anupama/1260022017",
+    },
+    {
+        sname: "Aarya 2",
+        imgsrc: "https://im.indiatimes.in/content/2020/Jul/sushmita-sen-aarya-1200_5f07071cf3026.jpg?w=725&h=402",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/aarya/1260028094",
+    },
+    {
+        sname: "Special Ops",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T132115708_5f324dfe31b60.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/special-ops/1260022894",
+    },
+    {
+        sname: "Criminal Justice",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T132331233_5f324eb465d43.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/criminal-justice/1260004267",
+    },
+    {
+        sname: "Hostages",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T132842706_5f324fce39b1c.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/hostages/1260005113",
+    },
+    {
+        sname: "Out of Love",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T133308393_5f3250d288311.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/out-of-love/1260013972",
+    }    
+];
+
+export default Sdata;
