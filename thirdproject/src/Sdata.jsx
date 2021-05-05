fa16965ddd994f6d51caf847d71896327bb69b32
Prince <pkrkinth@gmail.com>
2021-05-05 14:43:24 +0530

    Added thirdproject

---
+++ thirdproject/src/Sdata.jsx
@@ -0,0 +1,81 @@
+const Sdata = [
+    {
+        id: "1",
+        sname: "Anupama",
+        imgsrc: "https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/6769/826769-h",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/anupama/1260022017",
+    },
+    {
+        id: "2",
+        sname: "The Falcon And The Winter Soldier",
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BODNiODVmYjItM2MyMC00ZWQyLTgyMGYtNzJjMmVmZTY2OTJjXkEyXkFqcGdeQXVyNzk3NDUzNTc@._V1_.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/the-falcon-and-the-winter-soldier/1260055670",
+    },
+    {
+        id: "3",
+        sname: "Aarya 2",
+        imgsrc: "https://im.indiatimes.in/content/2020/Jul/sushmita-sen-aarya-1200_5f07071cf3026.jpg?w=725&h=402",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/aarya/1260028094",
+    },
+    {
+        id: "4",
+        sname: "Special Ops",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T132115708_5f324dfe31b60.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/special-ops/1260022894",
+    },
+    {
+        id: "5",
+        sname: "Criminal Justice",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T132331233_5f324eb465d43.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/criminal-justice/1260004267",
+    },
+    {
+        id: "6",
+        sname: "Hostages",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T132842706_5f324fce39b1c.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/hostages/1260005113",
+    },
+    {
+        id: "7",
+        sname: "Out of Love",
+        imgsrc: "https://im.indiatimes.in/content/2020/Aug/Untitled-design---2020-08-11T133308393_5f3250d288311.jpg",
+        title: "Disney Hotstar Original Series",
+        link: "https://www.hotstar.com/in/tv/out-of-love/1260013972",
+    },
+    {
+        id: "8",
+        sname: "Shadow and Bone",
+        imgsrc: "https://m.media-amazon.com/images/M/MV5BNTNhY2JlMmItMGMzZi00ZTM2LTlmNTItZDBkMGVhNGJhYWRhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
+        title: "Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80236319",
+    },
+    {
+        id: "9",
+        sname: "Pagglait",
+        imgsrc: "https://bingeddata.s3.amazonaws.com/uploads/2021/03/Pagglait-Netflix-Review.jpg",       
+        title: "Netflix Original Series",
+        link: "https://www.netflix.com/in/title/81242571",
+    },
+    {
+        id: "10",
+        sname: "The Boss Baby: Back in Business",
+        imgsrc: "https://occ-0-1722-1723.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABVMcj8FhRScXI0lgJDkT7SyXDCAqW_QS5wt__PaFxEI4R5fuKCDiChoeKICx08GuF-5klZiHUgkNh_bKcf1jGC17QXaIAO3zG52YeFXl60aLqTUFcfQh2_nQ6gz1_ATguyQHtlXToIelR-bJzWPS5aN9grMo020.jpg",
+        title: "Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80178943",
+    },
+    {
+        id: "11",
+        sname: "Trollhunters: Tales of Arcadia",
+        imgsrc: "https://variety.com/wp-content/uploads/2020/05/trollhunters-video-game.jpg",
+        title: "Netflix Original Series",
+        link: "https://www.netflix.com/in/title/80075820",
+    }
+];
+
+export default Sdata;
