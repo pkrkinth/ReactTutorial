adb880aaee44f8a43de049c614c07f31f131ceeb
Prince <pkrkinth@gmail.com>
2021-05-05 15:56:29 +0530

    added example - use of ternary operation

--- thirdproject/src/App.jsx
+++ thirdproject/src/App.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import Hotstar from './Hotstar';
 import Netflix from './Netflix';
 
-const favSeries = 'netflix';
+const favSeries = 'hotstar';
 
-const FavSeries = () => {
-  if (favSeries === 'hotstar') {
-    return <Netflix/>;
-  }else {
-    return <Hotstar/>;
-  }
-}
+// Example: how to use if/else
+// const FavSeries = () => {
+//   if (favSeries === 'hotstar') {
+//     return <Netflix/>;
+//   }else {
+//     return <Hotstar/>;
+//   }
+// }
 
 const App = () => (
   <>
-    <h1 className="heading_style">List of Top 5 Disney Hotstar Web Series in 2020</h1>
-    <FavSeries/>
+    <h1 className="heading_style">List of Top Web Series in 2020</h1>
+    {/* <FavSeries/> */}
+    {/* Below is example: how to use Ternary operation instaed of if/else  */}
+    {favSeries === 'netflix' ? <Netflix/> : <Hotstar/> }
   </>
 );
 
