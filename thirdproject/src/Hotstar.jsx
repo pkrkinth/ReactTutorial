afe24b41f5afc5342a7a15344d9dd78194278711
Prince <pkrkinth@gmail.com>
2021-05-07 14:42:39 +0530

    added Prettier extension for code formatting

--- thirdproject/src/Hotstar.jsx
+++ thirdproject/src/Hotstar.jsx
@@ -3,15 +3,15 @@ import Sdata from './Sdata';
 import Card from './Cards';
 
 const Hotstar = () => {
-    return (
-        <Card
-          key={Sdata[0].id}
-          sname={Sdata[0].sname}
-          imgsrc={Sdata[0].imgsrc}
-          title={Sdata[0].title}
-          link={Sdata[0].link}
-        />
-    );
+  return (
+    <Card
+      key={Sdata[0].id}
+      sname={Sdata[0].sname}
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      link={Sdata[0].link}
+    />
+  );
 }
 
 export default Hotstar;
