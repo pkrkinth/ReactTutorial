8c6dfa5a0f955c2652387bbb72f554b60d98d476
Prince <pkrkinth@gmail.com>
2021-05-10 23:59:02 +0530

    added Hooks example - counter,  show current time

--- counterapp/src/App.jsx
+++ counterapp/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 // Hooks example: useState
 const App = () => {
-  const state = useState();
+  // const state = useState();
 
   const [count, setCount] = useState(0);
   const IncNumber = () => {
@@ -10,10 +10,22 @@ const App = () => {
     setCount(count + 1);
   };
 
+  // Example of show current time with btn click or refresh the page
+  let currentTime = new Date().toLocaleTimeString();
+
+  const [cTime, setTime] = useState(currentTime);
+  const UpdateTime = () => {
+    let newCurrentTime = new Date().toLocaleTimeString();
+    setTime(newCurrentTime);
+  };
+
   return (
     <>
       <h1>{count}</h1>
       <button onClick={IncNumber}> Click Me </button>
+
+      <h1>{cTime}</h1>
+      <button onClick={UpdateTime}> Get Time</button>
     </>
   );
 };
