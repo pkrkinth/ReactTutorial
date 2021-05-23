c6e772381b4f13ffbea6ed396b270983982c6f82
Prince <pkrkinth@gmail.com>
2021-05-23 20:59:59 +0530

    Update the code using Form

--- fourthproject/src/App.jsx
+++ fourthproject/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-const App = () => {
+ // * Without using Form * //
+/*const App = () => {
     const [name, setName] = useState("");
     const [fullName, setFullName] = useState();
 
@@ -23,6 +24,53 @@ const App = () => {
         />
         <button onClick={onSubmit}>Click Me 👍</button>
     </div>
+    </>
+    );
+}*/
+
+// * Using Form * //
+const App = () => {
+    const [name, setName] = useState("");
+    const [firstName, setFirstName] = useState("");
+
+    const [lastName, setLastName] = useState("");
+    const [finalLastName, setFinalLastName] = useState("");
+
+    const InputEvent = (event) => {
+        console.log(event.target.value);
+        setName(event.target.value);
+    }
+
+    const secondInputEvent = (event) => {
+        console.log(event.target.value);
+        setLastName(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setFirstName(name);
+        setFinalLastName(lastName);
+    }
+
+    return (
+    <>
+        <div>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <h1> Hello {firstName} {finalLastName}</h1>
+                    <input type="text" placeholder="Enter your first name"
+                        onChange={InputEvent}
+                        value={name}
+                    />
+                    <br/>
+                    <input type="text" placeholder="Enter your last name"
+                        onChange={secondInputEvent}
+                        value={lastName}
+                    />
+                    <button type="submit">Click Me 👍</button>
+                    </div>
+                </form>
+        </div>
     </>
     );
 }
